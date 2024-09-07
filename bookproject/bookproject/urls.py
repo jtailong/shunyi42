937bf0180abebd91a15132684aa917cb702dbc34
jtailong <jtailong@163.com>
2024-09-07 17:12:47 +0800

    admin station

--- bookproject/bookproject/urls.py
+++ bookproject/bookproject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for bookmanager project.
+URL configuration for bookproject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
--- bookproject/bookproject/wsgi.py
+++ bookproject/bookproject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for bookmanager project.
+WSGI config for bookproject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookmanager.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookproject.settings')
 
 application = get_wsgi_application()
